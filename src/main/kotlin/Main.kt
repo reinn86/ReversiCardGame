547e0223e5e911e52b241ae62a7fb0812b7025db
TANUKI-LAPTOP\haruki <tanukichang@outlook.jp>
2023-05-18 13:59:26 +0900

    no message

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,8 @@
 
 import application.controller.TitleController
 import application.view.Window
-import domain.service.Configurable
 
 fun main() {
     Window.createWindow()
     TitleController.start()
-    val test = Configurable()
-    test.createConfigFile()
 }
