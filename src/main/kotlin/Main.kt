f1b7e32abe478d3b4eea5771ee4695e09ce1c1cc
haruki takahashi <tanukichang@outlook.jp>
2023-05-31 07:42:08 +0900

    ルールー作成続き

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,13 +6,7 @@ import domain.service.Reversi
 //}
 
 fun main() {
-    var tcount = 0
     val r = Reversi()
-    for(i in 0 .. 1000) {
-        if(r.decideTurn()) {
-            tcount++
-        }
-    }
 
-    println(tcount)
+    r.decideTurn()
 }
--- src/main/kotlin/domain/service/Reversi.kt
+++ src/main/kotlin/domain/service/Reversi.kt
@@ -4,13 +4,11 @@ class Reversi {
     private var isMyTurn = false
     private var troutX = 6
     private var troutY = 6
-    private var board  = Array(troutX) {
-        arrayOfNulls<Int>(  troutY)
-    }
+    private var board  = arrayOf<Array<Int>>()
 
-    private val STONE_EMPTY = "0"
-    private val STONE_BLACK = "1"
-    private val STONE_WHITE = "2"
+    private val STONE_EMPTY = 0
+    private val STONE_BLACK = 1
+    private val STONE_WHITE = 2
 
     constructor() {
         Reversi(6)
@@ -25,8 +23,18 @@ class Reversi {
         this.troutY = troutY
     }
 
-    fun initArrangement() {
-
+    fun initArrangement(board : Array<Array<Int>>) {
+        for (i in 0.. board.size){
+            for (j in 0.. board[i].size) {
+                if (j != STONE_EMPTY) {
+                    this.board[i][j] = board[i][j]
+                }
+            }
+        }
+        board[2][2] = STONE_BLACK
+        board[2][3] = STONE_WHITE
+        board[3][2] = STONE_WHITE
+        board[3][3] = STONE_BLACK
     }
 
     fun decideTurn(): Boolean {
