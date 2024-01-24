d8e4e1aed9025fc8fdd7b43b85404076377b8567
haruki takahashi <tanukichang@outlook.jp>
2024-01-25 02:21:26 +0900

    テストファイル修正

--- src/test/kotlin/Test1.kt
+++ src/test/kotlin/Test1.kt
@@ -1,12 +1,6 @@
-
-import domain.service.reversi.Timer
-import java.lang.Thread.sleep
-
 fun main() {
-    val a = Timer()
-    a.timer.start()
-    while (!a.isTimeCome) {
-        sleep(1000)
-    }
-    a.timer.start()
+    val name = "haruki"
+
+    println("Hello world!!")
+    println(name + "さんようこそ\nこれはkotlinです")
 }
